8f785ae2bc44c5d5fc0adb49cd8f33ca218a2b5c
incheol1024 <yaho1024@naver.com>
2020-01-17 21:00:10 +0900

    angel branch commit

--- src/main/java/me/incheol/chapter10/Question1.java
+++ src/main/java/me/incheol/chapter10/Question1.java
@@ -1,8 +1,33 @@
 package me.incheol.chapter10;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class Question1 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        findFilesIncludingWord("D:/app", "");
+    }
+
+
+    public static File[] findFilesIncludingWord(String pathDir, String word) throws IOException {
+
+        Path path = Paths.get(pathDir);
+        Stream<Path> walk = Files.walk(path, FileVisitOption.FOLLOW_LINKS);
+        walk.forEach(System.out::println);
+
+        return null;
+
+    }
+
 
 
 
