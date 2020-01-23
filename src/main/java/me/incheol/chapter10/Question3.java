0dfc186f7028ba7b9df3d3b04947658c42be919a
incheol1024 <yaho102466@gmail.com>
2020-01-23 23:41:11 +0900

    Add chapte10 question3

--- src/main/java/me/incheol/chapter10/Question3.java
+++ src/main/java/me/incheol/chapter10/Question3.java
@@ -4,18 +4,25 @@ import java.io.IOException;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 
-// 현재 풀이중입니다.
 public class Question3 {
 
     public static void main(String[] args) {
+        Question3 question3 = new Question3();
 
-
+        try {
+            question3.allocateTasks(Executors.newCachedThreadPool(), Paths.get("D:/app"), "test");
+        } catch (IOException | ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -45,13 +52,14 @@ public class Question3 {
         };
     }
 
+    // 해당 문자열에 원하는 단어가 몇번 나오는지 횟수를 리턴하는 메서드
     private int getNumberOfWord(String word, String value) {
-        int position=0;
+        int position = 0;
         int count = 0;
 
         while (true) {
             position = value.indexOf(word, position);
-            if(position != -1) {
+            if (position != -1) {
                 count++;
                 position = position + word.length();
                 continue;
@@ -63,15 +71,15 @@ public class Question3 {
 
 
     //디렉터리에 들어있는 모든 파일 각각에 태스크를 하나씩 스케줄링하라. 그중 하나가 성공하면 나머지 태스크를 모두 인터럽트해야 한다.
-    public void allocateTasks(Path path) throws IOException {
-
-        ExecutorService executorService = Executors.newCachedThreadPool();
-//        executorService.invokeAny()
-
+    public Integer allocateTasks(ExecutorService executorService, Path path, String word) throws IOException, ExecutionException, InterruptedException {
+        List<Callable<Integer>> callableList = null;
         if (Files.isDirectory(path)) {
-            Stream<Path> walk = Files.walk(path, 1, FileVisitOption.FOLLOW_LINKS);
+            callableList = Files.walk(path, 1, FileVisitOption.FOLLOW_LINKS)
+                    .filter(path1 -> !Files.isDirectory(path1))
+                    .map(path1 -> getWordFinderTask(path1, word))
+                    .collect(Collectors.toList());
 
         }
-
+        return executorService.invokeAny(callableList);
     }
 }
