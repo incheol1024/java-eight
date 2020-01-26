030c2170638c8ff25e30809ce2d40dfd710ada49
incheol1024 <yaho102466@gmail.com>
2020-01-26 15:27:18 +0900

    add Chapter10 question10

---
+++ src/main/java/me/incheol/chapter10/Question10.java
@@ -0,0 +1,12 @@
+package me.incheol.chapter10;
+
+// 블로킹큐로 디렉터리에 있는 파일을 처리하라. 한 스레드는 파일 트리를 순회하여 큐에 파일을 삽입한다.
+// 다른 여러 스레드는 파일을 제거하고 주어진 키워드에 해당하는 파일을 검색하며, 일치하는 파일을 출력한다.
+// 생산자는 작업을 마칠 때 큐에 더미 파일을 집어넣어야 한다.
+public class Question10 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
